e3048514f21e93598afcf2c21c4084cca49b029f
liguanghao <liguanghao@qingting.fm>
2021-01-06 10:02:36 +0800

    init

--- webhook.js
+++ webhook.js
@@ -3,7 +3,7 @@ let server = http.createServer(function(req, res){
     if(req.method == 'POST' && req.url == '/webhook'){
         // https://121.36.36.62:4000/webhook 向这个地址发送post请求
         res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({ok: true})) // 告诉服务器, 成功了
+        res.end(JSON.stringify({ok: true})) // 告诉github服务器, 成功了
     } else {
         res.end('Not Found')
     }
