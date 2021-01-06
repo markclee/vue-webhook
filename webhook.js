32795b74d5485fd520e9658243a235d64b9ca861
liguanghao <liguanghao@qingting.fm>
2021-01-06 16:37:38 +0800

    webhook及dockerShell

--- webhook.js
+++ webhook.js
@@ -1,8 +1,27 @@
 let http = require('http')
+let crypto  = require('crypto')
+let SECRET = '123456'
+function sign(body){
+    return 'sha1='+crypto.createHmac('sha1', SECRET).update(body).digest('hex');
+}   
 let server = http.createServer(function(req, res){
     if(req.method == 'POST' && req.url == '/webhook'){
         // https://121.36.36.62:4000/webhook 向这个地址发送post请求
         console.log(req.method, req.url)
+        // 拿到客户端发送过来的请求体
+        let buffers = []
+        req.on('data', function(buffer){
+            buffers.push(buffer)
+        })
+        req.on('end', function(bufffer){
+            let body = Buffer.concat(buffers)
+            let event = req.header['x-gitHub-event'] //小写 event=push
+            // github请求过来时候, 要传递请求body, 另外还会传一个signature过来, 你需要验证签名对不对
+            let signature = req.headers['x-hub-signature']
+            if(signature !== sign(body)){
+                return res.end('Not Allowed')
+            }
+        })
         res.setHeader('Content-Type', 'application/json')
         res.end(JSON.stringify({ok: true})) // 告诉github服务器, 成功了
     } else {
